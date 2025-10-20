0f8c4567df6513768d485db896765af5d02c57ab
Atharva Chandurkar <atharvachandurkar7115@gmail.com>
2025-10-20 18:29:53 +0530

    Update app.listen functionality

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
-const port =  5000 ;
+const port = process.env.PORT || 5000;
 connectToDatabase()
 .then(()=> {
   app.listen(port ,()=>{

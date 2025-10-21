abf55cc73435105e04729a7cfe34d59499165474
Atharva Chandurkar <atharvachandurkar7115@gmail.com>
2025-10-21 10:13:05 +0530

    changes in cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,36 +1,33 @@
 import express from "express";
-import {config} from "dotenv" ;
-import morgan from 'morgan' ;
+import { config } from "dotenv";
+import morgan from "morgan";
 import appRouter from "./routes/index.js";
-import cookieParser from "cookie-parser" ;
+import cookieParser from "cookie-parser";
 import cors from "cors";
+
 config();
-const app = express() ;
+const app = express();
+
+
 const allowedOrigins = [
   "http://localhost:5173",
-  process.env.FRONTEND_URL, 
-].filter(Boolean); 
+  process.env.FRONTEND_URL,  
+].filter(Boolean);
+
+
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow Postman / backend calls
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,   
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    optionsSuccessStatus: 200,
   })
 );
 
-app.use(express.json()) ;
-app.use(cookieParser(process.env.COOKIE_SECRET)) ;
-app.use(morgan("dev")) ;
-app.use("/api/v1" ,appRouter);
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan("dev"));
 
+app.use("/api/v1", appRouter);
 
-export default app ;
+export default app;
